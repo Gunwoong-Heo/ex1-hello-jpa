972dd54017e56953cd829e49e6bd7799f15b1c98
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-26 17:50:08 +0900

    Member 엔티티 추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,11 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+//@Table(name = "USER")   // DB 테이블 명이 다를 경우
 public class Member {
 
     @Id
     private Long id;
+//    @Column(name = "username")  // 테이블의 column명을 명시적으로 표기하는 경우
     private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
