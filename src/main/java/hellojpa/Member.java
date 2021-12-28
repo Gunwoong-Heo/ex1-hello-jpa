1ef8e15983971a86e6ecf8a7143c9ed075c56758
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-28 23:02:36 +0900

    `단방향 연관관계 예제` 추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,10 +9,40 @@ import java.util.Date;
 //@Table(name = "USER")   // DB 테이블 명이 다를 경우
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME", nullable = false)
     private String username;
 
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
